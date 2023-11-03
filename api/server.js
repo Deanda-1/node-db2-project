15e8f34cfc41d8735c2064229299aaecd4d40721
DJ1 <jimmynoble604@gmail.com>
2023-11-03 18:11:44 -0400

    server.js

--- api/server.js
+++ api/server.js
@@ -2,6 +2,14 @@ const express = require("express")
 
 const server = express()
 
-// DO YOUR MAGIC
+server.use(express.json())
+server.use('*', (req, res, next) => {
+    next({ status: 404, message: 'not found' })
+})
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: error.message                    
+    })
+})
 
 module.exports = server
