4ab7488088baa9755af955b6d90c6caa4c18de6f
DJ1 <jimmynoble604@gmail.com>
2023-11-03 14:51:52 -0400

    01-cars.js finished

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,38 @@
 // STRETCH
+const cars = [
+    {
+      vin: '1111111111111',
+      make: 'toyota',
+      module: 'prius',
+      mileage: 215000,
+      title: 'clean',
+      transmission: 'manual',
+    },
+    {
+      vin: '1111111111111',
+      make: 'toyota',
+      module: 'corolla',
+      mileage: 15000,
+      title: 'salvage',
+    },
+    {
+      vin: '1111111111111',
+      make: 'ford',
+      module: 'focus',
+      mileage: 15000,
+    },
+  ]
+  
+   exports.seed = function (knex) {
+      return knex('cars')
+        .truncate().then(() => {
+          return knex('cars') .insert(cars)
+    })
+   }
+  
+   // exports.seed = async function (knex) {
+  //  await knex('cars').truncate()
+  //  await knex('cars').insert(cars)
+  // }
+  
+  
