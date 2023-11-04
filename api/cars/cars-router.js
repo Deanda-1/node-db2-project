4756985a2cd68ade1f9dfbe3e5b4f039db7ea791
DJ1 <jimmynoble604@gmail.com>
2023-11-03 21:51:03 -0400

    cars-router.js

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const Car = require('./cars-model')
-const router = express.Router()
+const {
+    checkCarId,
+} = require('./cars-middleware')
+const router = express.Router
 
 router.get('/', async (req, res, next) => {
     try {
@@ -11,8 +14,8 @@ router.get('/', async (req, res, next) => {
     }
 })
 
-router.get('/:id', async (req, res, next) => {
-    res.json('getting car with id ${req.params.id}')
+router.get('/:id', checkCarId, async (req, res, next) => {
+    res.json(req.car)
 })
 
 router.post('/', async (req, res, next) => {
